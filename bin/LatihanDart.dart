f81ce36a95b1ddd718c7b16101a03e186f0ae8c0
Cheshvan7 <100284042+Cheshvan7@users.noreply.github.com>
2022-03-05 16:03:17 +0700

    Version 2 (ganjil)

--- bin/LatihanDart.dart
+++ bin/LatihanDart.dart
@@ -8,4 +8,59 @@ void main(List<String> arguments) {
 
   print("n = $n");
 
+  int ctr = 0;
+
+  int angka = 1;
+
+  for (int i = 1; i <= n; i++) {
+    //stdout.write("  $ctr");
+
+    if (i > 2 && i < (n / 2).round()) {
+      ctr++;
+    } else if (i > (n / 2).round() + 1) {
+      ctr--;
+    }
+
+    if (angka > 9) {
+      angka = 1;
+    }
+
+    //stdout.write(" $ctr ");
+
+    for (int j = 1; j <= n; j++) {
+      if (i == 1 || i == n) {
+        if (j == (n / 2).round()) {
+          stdout.write("$angka");
+        } else {
+          stdout.write("*");
+        }
+      } else {
+        if (i == (n / 2).round()) {
+          if (j == 1 || j == n || j == (n / 2).round()) {
+            stdout.write("$angka");
+          } else {
+            stdout.write(" ");
+          }
+        } else {
+          if (j == (n / 2).round()) {
+            stdout.write(" ");
+          } else if (j > (n / 2).round() && j <= (n / 2).round() + ctr) {
+            stdout.write(" ");
+          } else if (j < (n / 2).round() && j >= (n / 2).round() - ctr) {
+            stdout.write(" ");
+          } else if (j == (n / 2).round() - ctr - 1 ||
+              j == (n / 2).round() + 1 + ctr) {
+            stdout.write("$angka");
+          } else {
+            stdout.write("*");
+          }
+        }
+      }
+    }
+
+    angka++;
+
+    stdout.write(" $ctr ");
+    stdout.write("\n");
+  }
 }
